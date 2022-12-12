3b6f4b6783ed29bfb5fd1f35b31dbf856fb215dc
Gans <ganapathy.b@skeintech.com>
2022-12-12 12:15:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import * as dotenv from "dotenv";
+import "./socket.js";
+
+dotenv.config();
+
+var app = express();
+
+const port = process.env.PORT;
+
+app.use(bodyParser.json({ limit: "25MB" }));
+
+app.use(
+  bodyParser.urlencoded({
+    limit: "10mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+
+
+//socket Io connections
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+});  
+
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    console.error(`Error catched! ${err}`);
+  
+    const error = {
+      status: err.status || 500,
+      message: err.message,
+    };
+  
+    res.status(error.status).send(error);
+});
+
+function onError(error) {
+    if (error.syscall !== "listen") {
+      console.error(error);
+    }
+  
+    const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+      case "EADDRINUSE":
+        console.error(bind + " is already in use");
+        process.exit(1);
+      default:
+        console.error(error);
+    }
+  }
+  
+  function onListening() {
+    const addr = app.address();
+    const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+    console.log("\nListening on " + bind);
+  }
+  
+  //app.listen(port);
+  app.on("error", onError);
+  app.on("listening", onListening);
+  
+  //console.log("Server started on port " + port);
